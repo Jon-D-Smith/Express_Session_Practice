f94e0d1840f1c0b0c03de5b4ab145f6e4c5126fd
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-23 18:07:09 -0500

    Added more routes to practice with sessions

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const PORT = 3000;
 const session = require('express-session');
 
 app.use(session({
-    secret: "thisisnotagoodsecret"
+    secret: "thisisnotagoodsecret",
+    resave: false,
+    saveUninitialized: false
 }))
 
 app.get('/viewcount', (req, res) => {
@@ -14,6 +16,17 @@ app.get('/viewcount', (req, res) => {
         req.session.count = 1;
     }
     res.send(`You have viewed this page ${req.session.count} Times`)
+})
+
+app.get('/register', (req, res) => {
+    const { username = "Anonymous" } = req.query;
+    req.session.username = username;
+    res.redirect('/greet')
+})
+
+app.get('/greet', (req, res) => {
+    const { username } = req.session;
+    res.send(`Welcome back, ${username}`)
 })
 
 
